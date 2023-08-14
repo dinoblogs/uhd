34b63c7c40c29fcb27456448a524cd9941550293
Mukul Sharma <113527802+dinoblogs@users.noreply.github.com>
2023-08-14 14:59:08 +0530

    Update movie.js

--- static/js/movie.js
+++ static/js/movie.js
@@ -1,14 +1,17 @@
 // Get all elements with the class "mb-text"
-var mbTextElements = document.querySelectorAll('.mb-text');
+var mbTextElements = document.querySelectorAll('.maxbutton');
 
 // Iterate through each element and modify its classes
-mbTextElements.forEach(function (element) {
-    // Remove the class "mb-text"
-    element.classList.remove('mb-text');
-
-    // Add the classes "btn" and "btn-success"
-    element.classList.add('btn', 'btn-success');
-});
+mbTextElements.forEach(function(element, index) {
+    element.style.margin = '10px'
+    if (index % 2 === 0) {
+      // Even index, add classes "btn" and "btn-success"
+      element.classList.add('btn', 'btn-success');
+    } else {
+      // Odd index, add classes "btn" and "btn-primary"
+      element.classList.add('btn', 'btn-primary');
+    }
+  });
 
 // Get all <p> elements
 var pElements = document.querySelectorAll('p');
